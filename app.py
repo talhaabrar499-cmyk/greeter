90e7a28b4686e29778e43114f0462daa230b30f4
https://github.com/talhaabrar499-cmyk <talhaabrar499@gmail.com>
2026-09-21 09:58:42 +0500

    Initial greeter app

---
+++ app.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class GreeterHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = """
+        <html>
+            <head>
+                <title>Greeter App</title>
+            </head>
+            <body>
+                <h1>Hello, Talha!</h1>
+                <p>Welcome to my containerized Python application.</p>
+            </body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+
+server = HTTPServer(("0.0.0.0", 8000), GreeterHandler)
+
+print("Greeter app running on http://localhost:8000")
+
+server.serve_forever()
